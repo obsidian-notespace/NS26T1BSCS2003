dc5d43cba3c9a89da9128132fe62346f3d5576e9
Harsh <23f3002742@ds.study.iitm.ac.in>
2026-04-09 20:54:11 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,23 @@
+window.htmlFiles = [
+    {
+        "path": "sol/solution/WEEK 05/AQ 5.1_ Activity Question 1 - Not Graded __ IITM Online Degree.html"
+    },
+    {
+        "path": "sol/solution/WEEK 05/AQ 5.2_ Activity Question 2 - Not Graded __ IITM Online Degree.html"
+    },
+    {
+        "path": "sol/solution/WEEK 05/AQ 5.3_ Activity Question 3 - Not Graded __ IITM Online Degree.html"
+    },
+    {
+        "path": "sol/solution/WEEK 05/AQ 5.4_ Activity Question 4 - Not Graded __ IITM Online Degree.html"
+    },
+    {
+        "path": "sol/solution/WEEK 05/AQ 5.5_ Activity Question 5 - Not Graded __ IITM Online Degree.html"
+    },
+    {
+        "path": "sol/solution/WEEK 05/Graded Assignment 5 __ IITM Online Degree.html"
+    },
+    {
+        "path": "sol/solution/WEEK 05/Practice Assignment 5 - Not Graded __ IITM Online Degree.html"
+    }
+];
